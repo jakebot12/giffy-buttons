eff619d16b978220f57a3b93515c8985ce99fdbf
Jake Gurley <jakemg12@gmail.com>
2019-03-25 23:16:24 -0700

    <COMMENT>

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//my javascript code will go below here
+//initialize global variables
+//create initial string of themed buttons
+//set up ajax call for giff retrieval
+//set up a function for retrieving the giff content based on the context of the buttons
+//create a function for appendinding new buttons
